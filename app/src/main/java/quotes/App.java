d4acec9b3ac806a3304cd78953337ae4ec52166e
Alex Carr <mralex.carr@yahoo.com>
2023-06-22 19:45:01 -0700

    Updates

--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -1,28 +1,15 @@
 package quotes;
 
+import java.io.File;
 import java.io.IOException;
 
 public class App {
+    public static void main(String[] args) throws IOException {
+        File filePath = new File("src/main/resources/recentquotes.json");
+        QuoteReader quoteReader = new QuoteReader(filePath);
+        Quote randQuote = quoteReader.getRandomQuote();
 
-    public static void main(String[] args) {
-        QuotesReader quotesReader = new QuotesReader("./app/src/main/resources/recentquotes.json");
-        Quote quote;
-
-        try {
-            quote = quotesReader.getRandomStarChar();
-        } catch (IOException e) {
-            System.out.println("Failed to fetch Star Wars character from API. Falling back to local JSON file.");
-            quote = quotesReader.getRandomQuote();
-        }
-
-        displayQuote(quote);
-    }
-
-    public static void displayQuote(Quote quote) {
-        if (quote != null) {
-            System.out.println(quote);
-        } else {
-            System.out.println("No quotes available.");
-        }
+        System.out.println("Quote: " + randQuote.getText());
+        System.out.println("Author: " + randQuote.getAuthor());
     }
 }
